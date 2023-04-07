8ae048e39f024f50744eff418832ce5439899097
slivcaigor <slivcaigor@gmail.com>
2023-04-07 16:58:59 +0200

    Add project files.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,49 @@
+﻿namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        // Attributo privato readonly per il codice del prodotto, che viene generato casualmente alla creazione del prodotto
+        private readonly int _codice;
+        // Proprietà pubblica per ottenere il codice del prodotto (solo in lettura)
+        public int Codice => _codice;
+        // Proprietà pubbliche per nome, descrizione, prezzo e iva del prodotto (sia in lettura che in scrittura)
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public int Iva { get; set; }
+
+        // Creo un costruttore della classe Prodotto che prende come argomenti il nome, la descrizione, il prezzo e l'iva del prodotto
+        public Prodotto(string nome, string descrizione, decimal prezzo, int iva)
+        {
+            // Genero in modo casuale il codice del prodotto
+            _codice = new Random().Next(10000, 99999);
+            // Inizializzo le proprietà nome, descrizione, prezzo e iva con i valori passati come argomento
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+            Iva = iva;
+        }
+
+        // Creo il metodo PrezzoBase che restituisce il prezzo base del prodotto (senza iva)
+        public decimal PrezzoBase()
+        {
+            return Prezzo;
+        }
+
+        // Creo il metodo PrezzoConIva che calcola il prezzo del prodotto comprensivo di iva
+        public decimal PrezzoConIva()
+        {
+            // Calcolo del prezzo con iva aggiunto
+            decimal prezzoConIva = Prezzo + Prezzo * Iva / 100m;
+            // Arrotondamento a due cifre decimali
+            return decimal.Round(prezzoConIva, 2);
+        }
+
+        // Creo il metodo NomeEsteso che restituisce il nome esteso del prodotto, ottenuto concatenando il codice del prodotto con il nome
+        public string NomeEsteso()
+        {
+            return Codice + " - " + Nome;
+        }
+
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+BONUS:
+- create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+- Usando un array, dichiarate un elenco dei prodotti di un negozio e inseriteci al suo interno qualche prodotto che vi aspettate di trovare nel negozio. Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.
+*/
+
+namespace csharp_oop_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creo un nuovo oggetto Prodotto, specificando nome, descrizione, prezzo e iva
+            Prodotto nft = new("CryptoKitty", "Un gatto digitale unico", 3200m, 22);
+
+            // Stampo le informazioni sul prodotto utilizzando il metodo Console.WriteLine()
+            Console.WriteLine($"Codice: {nft.Codice}\nNome: {nft.Nome}\nDescrizione: {nft.Descrizione}\nPrezzo Base: ${nft.PrezzoBase()}\nPrezzo Ivato: ${nft.PrezzoConIva()}\nNome Esteso: {nft.NomeEsteso()}");
+
+        }
+    }
+}
